a02fb475f29da9ec845be6b21df7612381d22a79
lautaroandradaet2121-spec <lautaro.andrada.et21.21@gmail.com>
2025-10-09 07:57:41 -0300

    R

--- tiny-boxing/items.gd
+++ tiny-boxing/items.gd
@@ -1,4 +1,15 @@
 class_name items extends RigidBody2D
+@onready var eliminador = $eliminacion_items/StaticBody2D
+#const eliminador_path = preload("res://eliminacion_items.tscn")
 @onready var items_el = $eliminacion_items2D
 
 @export var posision = Vector2(0, 0)
+
+func _physics_process(delta: float) -> void:
+	var item_col = $RayCast2D
+	var item_a_eliminar = $items
+	if item_col.is_colliding():
+		print("Esta colisionando con ", item_col.get_collider())
+		if item_col.get_collider() == eliminador:
+			print("Si colisiono")
+			remove_child(item_a_eliminar)
--- tiny-boxing/main.gd
+++ tiny-boxing/main.gd
@@ -1,6 +1,7 @@
 extends Node
 const item_1 = preload("res://items.tscn")
-@onready var eliminador = $eliminacion_items
+@onready var eliminador = $eliminacion_items/StaticBody2D
+
 func crear_item():
 	var nuevo_item = item_1.instantiate()
 	
@@ -9,15 +10,12 @@ func crear_item():
 	nuevo_item.position = Vector2(posicion_x, -10)
 	
 	add_child(nuevo_item)
-	
-	
-func _physics_process(delta: float) -> void:
-	if eliminador.is_colliding():
-		if eliminador.get_collider().name == "items":
-			remove_child(eliminador.get_collider())
-
 
 func _process(delta: float) -> void:
+	
 	crear_item()
 	
+func _ready() -> void:
+	pass
+#	print (eliminador)	
 	
