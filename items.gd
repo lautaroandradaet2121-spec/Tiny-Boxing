6cc69115b476c60b2d0a209f3fc65383ea6a1ef8
lautaroandradaet2121-spec <lautaro.andrada.et21.21@gmail.com>
2025-11-20 08:42:48 -0300

    added lambda

--- items.gd
+++ items.gd
@@ -4,14 +4,19 @@ func accion(body):
 	pass
 
 func _ready() -> void:
-	self.body_entered.connect(_on_body_entered)
+	#self.body_entered.connect(_on_body_entered)
+	self.body_entered.connect(func(body):
+		if body.is_in_group("jugador"):
+			self.accion(body)
+			self.queue_free()
+		)
 
-func _on_body_entered(body: Node2D):
-	print("colisiona")
+#func _on_body_entered(body: Node2D):
+#	print("colisiona")
 		
-	if body.is_in_group("jugador"):
-		self.accion(body)
-		self.queue_free()
+#	if body.is_in_group("jugador"):
+#		self.accion(body)
+#		self.queue_free()
 		
 func _physics_process(delta: float) -> void:
 	if position.y >= 700:
