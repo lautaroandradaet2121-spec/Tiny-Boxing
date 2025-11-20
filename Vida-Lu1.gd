c91fcc92dd2ecba545235c55a6c55a481e50052e
lautaroandradaet2121-spec <lautaro.andrada.et21.21@gmail.com>
2025-11-19 21:26:39 -0300

    fixed health bar

--- Vida-Lu1.gd
+++ Vida-Lu1.gd
@@ -1,6 +1,7 @@
 extends TextureProgressBar
 
 #const imagen_atk_2_j2 = preload("res://img/js animations/j2/atk_2_j2.png")
+var barra_ver = load("res://img/color_barra.png")
 var barra_ama = load("res://img/barra_vida_ama.png")
 var barra_roj = load("res://img/barra_vida_roj.png")
 #func _ready() -> void:
@@ -9,6 +10,9 @@ var barra_roj = load("res://img/barra_vida_roj.png")
 func actualizar_barra():
 	var luchador = get_tree().get_nodes_in_group("jugador2")
 	for lucha in luchador:
+		if lucha.vida > 5:
+			self.texture_progress = barra_ver
+			
 		if lucha.vida <= 5 and lucha.vida > 2:
 			self.texture_progress = barra_ama
 		
